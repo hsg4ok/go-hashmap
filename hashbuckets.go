31718f365454b7f1367fae3106b66899e8bebf4c
Peter Froehlich <peter.hans.froehlich@gmail.com>
2009-12-19 21:32:33 -0500

    Experimental hashArray without separate chaining.

---
+++ hashbuckets.go
@@ -0,0 +1,106 @@
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package hashmap
+
+// Buckets start out fresh; once they contain a
+// pair they become used; once their pair is
+// removed they become deleted.
+const (
+	fresh = iota;
+	used;
+	deleted;
+)
+
+// Buckets are just HashPairs and a state.
+type bucket struct {
+	pair HashPair
+	state int
+}
+
+// Special bucket written to array for deleted buckets.
+var deletedBucket bucket = bucket{HashPair{nil, nil}, deleted}
+
+// All we do is wrap a bucket slice. Yes there's a pointer
+// indirection, sue me.
+type bucketArray struct {
+	data []bucket;
+}
+
+// Find the bucket index for the given key. If the bucket
+// used, we found an exact match; if the bucket is fresh
+// or deleted, we can insert there. Note that we relocate
+// buckets on successful searches (if possible).
+func (self *bucketArray) find(key Hashable) (index int) {
+	d := self.data
+	l := uint(len(d))
+	h := key.Hash() % l
+	i := h
+	r := -1 // relocation index
+	for {
+		b := d[i]
+
+		switch b.state {
+		case fresh:
+			// key not found
+			if r != -1 {
+				return r
+			}
+			return int(i)
+		case deleted:
+			// remember if it's the first
+			if r == -1 {
+				r = int(i)
+			}
+		case used:
+			// winner if keys match
+			if key.Equal(b.pair.Key) {
+				// relocate from i to r if possible
+				if r != -1 {
+					d[r] = b
+					d[i] = deletedBucket
+					return r
+				}
+				// otherwise just return it
+				return int(i)
+			}
+		}
+
+		// next index, wrapping around
+		i = (i + 1) % l
+
+		// back to where we started?
+		if i == h {
+			if r != -1 {
+				// if we have a deleted one, return that
+				return r
+			} else {
+				// table full, should never happen
+				panic("bucketArray.find: table full")
+			}
+		}
+	}
+
+	// unreachable
+	panic("bucketArray.find: unreachable")
+	return -1
+}
+
+func (self *bucketArray) push(key Hashable, value interface{}) bool {
+	p := self.find(key)
+	if self.data[p].state == used {
+		return false
+	}
+	self.data[p] = bucket{HashPair{key, value}, used}
+	return true
+}
+
+func (self *bucketArray) pop(key Hashable) bool {
+	p := self.find(key)
+	if self.data[p].state != used {
+		return false
+	}
+	self.data[p] = deletedBucket
+	return true
+}
